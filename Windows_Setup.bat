bf85806d5b935db54b55a6000a98a0a88f56bea7
Yamin Noor <ymnoor21@gmail.com>
2018-08-06 20:55:33 -0700

    Add windows 64 bit python 3.7 version

--- Windows_Setup.bat
+++ Windows_Setup.bat
@@ -9,5 +9,14 @@ CALL ".\venv\Scripts\activate"
 
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
 
+reg Query "HKCU\Software\Python\PythonCore\3.6" | find /i "3.6" > NUL && set py=36 || set py=27
+reg Query "HKCU\Software\Python\PythonCore\3.7" | find /i "3.7" > NUL && set py=37 || set py=27
+
 if %OS%==32BIT pip install -r requirements-win32-py36.txt
-if %OS%==64BIT pip install -r requirements-win64-py36.txt
+if %OS%==64BIT (
+    if %py%==37 (
+        pip install -r requirements-win64-py37.txt
+    ) else (
+        pip install -r requirements-win64-py36.txt
+    )
+)
