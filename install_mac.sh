da06a07373f5c46fe5b6dd46116723d36d8a4d90
CoffeeBlend <jonasboettiger@icloud.com>
2018-09-08 17:03:09 +0200

    Added macOS installation instructions and installer script

---
+++ install_mac.sh
@@ -0,0 +1,9 @@
+# Install dependencies
+brew install python gdal spatialindex
+
+# Install pyproj
+pip3 install cython
+pip3 install git+https://github.com/jswhit/pyproj.git
+
+# Install other dependencies
+pip3 install numpy shapely rtree pillow requests
