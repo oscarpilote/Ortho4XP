b55a24b3ff4e8d8981d94263a7ba4749788a4a42
oscarpilote <oscar.pilote@gmail.com>
2018-10-18 15:35:23 +0200

    7z binary, not installer... (bis)

--- src/O4_Mesh_Utils.py
+++ src/O4_Mesh_Utils.py
@@ -23,7 +23,7 @@ elif 'win' in sys.platform:
     Triangle4XP_cmd = os.path.join(FNAMES.Utils_dir,"Triangle4XP.exe ")
     triangle_cmd    = os.path.join(FNAMES.Utils_dir,"triangle.exe ")
     sort_mesh_cmd   = os.path.join(FNAMES.Utils_dir,"moulinette.exe ")
-    unzip_cmd       = os.path.join(FNAMES.Utils_dir,"7z1805-x64.exe ")
+    unzip_cmd       = os.path.join(FNAMES.Utils_dir,"7z.exe ")
 else:
     Triangle4XP_cmd = os.path.join(FNAMES.Utils_dir,"Triangle4XP ")
     triangle_cmd    = os.path.join(FNAMES.Utils_dir,"triangle ")
