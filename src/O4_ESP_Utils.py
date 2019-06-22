b7d2e6e7e4d8c52ca93cc5688bbc3ff4b31f4dc2
stackTom <stackTom@users.noreply.github.com>
2019-06-22 11:24:50 -0400

    add print to help debug scenproc issues

--- src/O4_ESP_Utils.py
+++ src/O4_ESP_Utils.py
@@ -320,6 +320,7 @@ def build_for_ESP(build_dir, tile):
         scenproc_thread = Thread(target=run_scenproc_threaded, args=(q2, ))
         scenproc_thread.daemon = True
         scenproc_thread.start()
+        print("Running ScenProc...")
         q2.put_nowait([scenproc_script_file, scenproc_osm_file_name, texture_folder])
         
     for (dirpath, dir_names, file_names) in os.walk(build_dir):
