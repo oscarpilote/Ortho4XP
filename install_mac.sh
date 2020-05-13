395b8f045a69bd76db524e6c65c48cf7e17d19ae
OldSubSailor <54075170+OldSubSailor@users.noreply.github.com>
2020-05-13 09:09:12 -0500

    Update install_mac.sh
    An effort to streamline installing Ortho4XP on a Mac. Updating the Homebrew master install.

--- install_mac.sh
+++ install_mac.sh
@@ -1,3 +1,13 @@
+# Install the latest version of Python on your Mac. 
+# Download and install Ortho4XP on your Mac.
+# Go to the Ortho4XP folder  cd /path/to/Ortho4XP 
+# THEN run this script.
+
+# Install Homebrew Master (ruby install is depreciated)
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+
+
 # Install dependencies
 brew install python gdal spatialindex p7zip
 
