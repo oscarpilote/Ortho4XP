4c28b38564c04c614aeff71191dfca427a5c1628
OldSubSailor <54075170+OldSubSailor@users.noreply.github.com>
2021-08-01 17:31:33 -0500

    Update install_mac.sh
    Catching up to Big Sur. This will work for almost any non-legacy macOS.

--- install_mac.sh
+++ install_mac.sh
@@ -1,9 +1,10 @@
 # Install dependencies
-brew install python gdal spatialindex p7zip
+# Changes as of Big Sur
+brew install  gdal spatialindex p7zip
 
 # Install pyproj
 pip3 install cython
-pip3 install git+https://github.com/jswhit/pyproj.git
+pip3 install pyproj
 
 # Install other dependencies
 pip3 install numpy shapely rtree pillow requests
