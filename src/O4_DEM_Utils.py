2edac911cda719370799c8ef003dac7a71420c68
Ryan Romanchuk <rromanchuk@gmail.com>
2021-09-14 12:20:08 -0700

    updates from thread v2

--- src/O4_DEM_Utils.py
+++ src/O4_DEM_Utils.py
@@ -7,22 +7,20 @@ import itertools
 from math import sqrt
 import array
 import numpy
+from PIL import Image
+import O4_UI_Utils as UI
+import O4_File_Names as FNAMES
+
 has_gdal = False
 try:
     from osgeo import gdal
     has_gdal = True
 except ImportError:
-    pass
-if not has_gdal:
     try:
         import gdal
         has_gdal = True
     except ImportError:
         pass
-
-from PIL import Image
-import O4_UI_Utils as UI
-import O4_File_Names as FNAMES
 
 available_sources=(
                  'View','Viewfinderpanoramas (J. de Ferranti) - mostly worldwide',
