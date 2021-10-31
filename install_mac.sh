e5996a62bceb766e949c467f51e091163621a70d
OldSubSailor <54075170+OldSubSailor@users.noreply.github.com>
2021-10-31 11:45:26 -0500

    Update install_mac.sh

--- install_mac.sh
+++ install_mac.sh
@@ -1,9 +1,23 @@
+# EDIT TO GET GDAL AND ORTHO UP TO DATE WITH CURRENT Github HOMEBREW FILES
+
+
 # Install dependencies
-brew install python gdal spatialindex p7zip
+brew install  gdal spatialindex p7zip
+# if only using Homebrew Python, then 
+	brew install python-tk@3.9
+	
+#  GDAL 3.1.
+# There are five major modules that are included with the GDAL Python bindings.:
 
-# Install pyproj
-pip3 install cython
-pip3 install git+https://github.com/jswhit/pyproj.git
+# >>> from osgeo import gdal	
 
-# Install other dependencies
-pip3 install numpy shapely rtree pillow requests
+# Install python packages
+python3 -m pip install --upgrade pip setuptools wheel
+python3 -m pip install cython --user
+python3 -m pip install pyproj --user
+python3 -m pip install numpy --user
+python3 -m pip install shapely --user
+python3 -m pip install rtree --user
+python3 -m pip install pillow --user
+python3 -m pip install requests --user
+ 
