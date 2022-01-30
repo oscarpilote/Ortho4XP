f46edf949e517d9c91425c0c8a59055f12176e20
Marc Saric <marc.saric@gmx.de>
2022-01-30 23:04:06 +0100

    Paint it black

--- Ortho4XP_v130.py
+++ Ortho4XP_v130.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import sys
 import os
-Ortho4XP_dir = '..' if getattr(sys, 'frozen', False) else '.'
-sys.path.append(os.path.join(Ortho4XP_dir, 'src'))
+
+Ortho4XP_dir = ".." if getattr(sys, "frozen", False) else "."
+sys.path.append(os.path.join(Ortho4XP_dir, "src"))
 
 import O4_File_Names as FNAMES
+
 sys.path.append(FNAMES.Provider_dir)
 import O4_Imagery_Utils as IMG
 import O4_Vector_Map as VMAP
@@ -16,18 +18,29 @@ import O4_Config_Utils as CFG  # CFG imported last because it can modify other m
 
 cmd_line = "USAGE: Ortho4XP_v130.py lat lon imagery zl (won't read a tile config)\n   OR:  Ortho4XP_v130.py lat lon (with existing tile config file)"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if not os.path.isdir(FNAMES.Utils_dir):
         print("Missing ", FNAMES.Utils_dir, "directory, check your install. Exiting.")
-        sys.exit()   
-    for directory in (FNAMES.Preview_dir, FNAMES.Provider_dir, FNAMES.Extent_dir, FNAMES.Filter_dir, FNAMES.OSM_dir,
-                      FNAMES.Mask_dir, FNAMES.Imagery_dir, FNAMES.Elevation_dir, FNAMES.Geotiff_dir, FNAMES.Patch_dir,
-                      FNAMES.Tile_dir, FNAMES.Tmp_dir):
+        sys.exit()
+    for directory in (
+        FNAMES.Preview_dir,
+        FNAMES.Provider_dir,
+        FNAMES.Extent_dir,
+        FNAMES.Filter_dir,
+        FNAMES.OSM_dir,
+        FNAMES.Mask_dir,
+        FNAMES.Imagery_dir,
+        FNAMES.Elevation_dir,
+        FNAMES.Geotiff_dir,
+        FNAMES.Patch_dir,
+        FNAMES.Tile_dir,
+        FNAMES.Tmp_dir,
+    ):
         if not os.path.isdir(directory):
-            try: 
+            try:
                 os.makedirs(directory)
                 print("Creating missing directory", directory)
-            except: 
+            except:
                 print("Could not create required directory", directory, ". Exit.")
                 sys.exit()
     IMG.initialize_extents_dict()
@@ -37,31 +50,35 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:  # switch to the graphical interface
         Ortho4XP = GUI.Ortho4XP_GUI()
 
-        Ortho4XP.mainloop()	    
+        Ortho4XP.mainloop()
         print("Bon vol!")
-    else:  # sequel is only concerned with command line 
+    else:  # sequel is only concerned with command line
         if len(sys.argv) < 3:
-            print(cmd_line); sys.exit()
+            print(cmd_line)
+            sys.exit()
         try:
             lat = int(sys.argv[1])
             lon = int(sys.argv[2])
         except:
-            print(cmd_line); sys.exit()
+            print(cmd_line)
+            sys.exit()
         if len(sys.argv) == 3:
             try:
-                tile = CFG.Tile(lat, lon, '')
+                tile = CFG.Tile(lat, lon, "")
             except Exception as e:
                 print(e)
-                print("ERROR: could not read tile config file."); sys.exit()
+                print("ERROR: could not read tile config file.")
+                sys.exit()
         else:
             try:
                 provider_code = sys.argv[3]
                 zoomlevel = int(sys.argv[4])
-                tile = CFG.Tile(lat, lon, '')
+                tile = CFG.Tile(lat, lon, "")
                 tile.default_website = provider_code
                 tile.default_zl = zoomlevel
             except:
-                print(cmd_line); sys.exit()
+                print(cmd_line)
+                sys.exit()
         try:
             VMAP.build_poly_file(tile)
             MESH.build_mesh(tile)
@@ -70,4 +87,3 @@ if __name__ == '__main__':
             print("Bon vol!")
         except:
             print("Crash!")
-        
--- src/O4_UI_Utils.py
+++ src/O4_UI_Utils.py
@@ -2,7 +2,7 @@ import os
 import sys
 import time
 
-Ortho4XP_dir = '..' if getattr(sys, 'frozen', False) else '.'
+Ortho4XP_dir = ".." if getattr(sys, "frozen", False) else "."
 verbosity = 1
 red_flag = False
 is_working = False
@@ -12,16 +12,20 @@ log = True
 
 
 ##############################################################################
-def progress_bar(nbr, percentage, message=None): 
+def progress_bar(nbr, percentage, message=None):
     if gui:
         gui.pgrbv[nbr].set(percentage)
+
+
 ##############################################################################
 
 
 ##############################################################################
 def vprint(min_verbosity, *args):
-    if verbosity >= min_verbosity: 
+    if verbosity >= min_verbosity:
         print(*args)
+
+
 ##############################################################################
 
 
@@ -29,63 +33,82 @@ def vprint(min_verbosity, *args):
 def logprint(*args):
     try:
         f = open(os.path.join(Ortho4XP_dir, "Ortho4XP.log"), "a")
-        f.write(time.strftime("%c") + ' | ' + ' '.join([str(x) for x in args]) + "\n")
+        f.write(time.strftime("%c") + " | " + " ".join([str(x) for x in args]) + "\n")
         f.close()
     except:
         pass
+
+
 ##############################################################################
 
 
 ##############################################################################
 def lvprint(min_verbosity, *args):
-    if verbosity >= min_verbosity: 
+    if verbosity >= min_verbosity:
         print(*args)
-    if log: logprint(*args)
+    if log:
+        logprint(*args)
+
+
 ##############################################################################
 
 
 ##############################################################################
 def bug_report(*args):
     logprint("An internal error occured. Please file a bug with lat/lon and cfg")
-    if args: logprint(*args)
+    if args:
+        logprint(*args)
+
+
 ##############################################################################
 
 
 ##############################################################################
 def exit_message_and_bottom_line(*args):
     global is_working
-    if not args: args = ("Process interrupted",)  
+    if not args:
+        args = ("Process interrupted",)
     if args[0]:
         logprint(*args)
         print(*args)
-    print('_____________________________________________________________' + \
-            '____________________________________')
+    print(
+        "_____________________________________________________________"
+        + "____________________________________"
+    )
     is_working = False
+
+
 ##############################################################################
 
 
 ##############################################################################
 def timings_and_bottom_line(tinit):
     global is_working
-    print('\nCompleted in ' + nicer_timer(time.time() - tinit) + '.')
-    print('_____________________________________________________________' + \
-            '____________________________________')
+    print("\nCompleted in " + nicer_timer(time.time() - tinit) + ".")
+    print(
+        "_____________________________________________________________"
+        + "____________________________________"
+    )
     is_working = False
+
+
 ##############################################################################
 
 
 ##############################################################################
-def human_print(num, suffix=''):
-    for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
+def human_print(num, suffix=""):
+    for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
         if abs(num) < 1024.0:
-            return '{:.1f}{}{}'.format(num, unit, suffix)
+            return "{:.1f}{}{}".format(num, unit, suffix)
         num /= 1024.0
-    return '{:.1f}{}{}'.format(num, 'Y', suffix)
+    return "{:.1f}{}{}".format(num, "Y", suffix)
+
+
 ##############################################################################
 
 
 def nicer_timer(elapsed):
-    out_string = ''
+    out_string = ""
     hours = elapsed // 3600
     if hours:
         elapsed -= 3600 * hours
@@ -94,6 +117,6 @@ def nicer_timer(elapsed):
     if hours or minutes:
         elapsed -= 60 * minutes
         out_string += str(int(minutes)) + "m"
-    elapsed = '{:.2f}'.format(elapsed) if not out_string else int(elapsed)
-    out_string += str(elapsed) + 'sec'
+    elapsed = "{:.2f}".format(elapsed) if not out_string else int(elapsed)
+    out_string += str(elapsed) + "sec"
     return out_string
