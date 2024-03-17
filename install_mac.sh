c7902870648eb479afe49d15ca0d71a80ee8921b
scriptPilot <dennis@scheffel.de>
2024-03-17 02:32:33 +0100

    Update install_mac.sh

--- install_mac.sh
+++ install_mac.sh
@@ -1,12 +1,5 @@
-# Install Python if not found
-if command -v python3 &>/dev/null; then
-    echo "python3 already available"
-else
-    brew install python
-fi
-
 # Install dependencies
-brew install gdal spatialindex p7zip proj python-tk
+brew install gdal spatialindex p7zip proj
 
 # Install pyproj
 pip3 install pyproj
