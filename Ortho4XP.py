542c482b5c77814adbe99e433a12efad7bfd3289
shred86 <shred86@icloud.com>
2026-03-29 14:29:04 -0700

    fix for proj.db issue

--- Ortho4XP.py
+++ Ortho4XP.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 import sys
 import os
+
+Ortho4XP_dir = '..' if getattr(sys, 'frozen', False) else '.'
+
+if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+    _proj_data_path = os.path.join(sys._MEIPASS, "pyproj", "proj_dir", "share", "proj")
+    _lib_path = os.path.join(sys._MEIPASS, "_internal")
+    os.environ["PROJ_DATA"] = _proj_data_path
+    os.environ["DYLD_LIBRARY_PATH"] = _lib_path + ":" + os.environ.get("DYLD_LIBRARY_PATH", "")
+
 from pyproj import datadir
-Ortho4XP_dir='..' if getattr(sys,'frozen',False) else '.'
-sys.path.append(os.path.join(Ortho4XP_dir,'src'))
+
+if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+    datadir.set_data_dir(_proj_data_path)
+
+sys.path.append(os.path.join(Ortho4XP_dir, 'src'))
 
 import O4_File_Names as FNAMES
 sys.path.append(FNAMES.Provider_dir)
@@ -15,61 +27,51 @@ import O4_Tile_Utils as TILE
 import O4_GUI_Utils as GUI
 import O4_Config_Utils as CFG  # CFG imported last because it can modify other modules variables
 
-# Check if running as a PyInstaller bundle and set PROJ_DATA environment
-if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
-    base_path = sys._MEIPASS
-    relative_proj_path = os.path.join("pyproj", "proj_dir", "share", "proj")
-    lib_path = os.path.join(sys._MEIPASS, "_internal")
-    os.environ["DYLD_LIBRARY_PATH"] = lib_path + ":" + os.environ.get("DYLD_LIBRARY_PATH", "")
-    proj_data_path = os.path.join(base_path, relative_proj_path)
-    os.environ["PROJ_DATA"] = proj_data_path
-    datadir.set_data_dir(proj_data_path)
-
-cmd_line="USAGE: Ortho4XP.py lat lon imagery zl (won't read a tile config)\n  OR:  Ortho4XP.py lat lon (with existing tile config file)"
+cmd_line = "USAGE: Ortho4XP.py lat lon imagery zl (won't read a tile config)\n  OR:  Ortho4XP.py lat lon (with existing tile config file)"
 
 if __name__ == '__main__':
     if not os.path.isdir(FNAMES.Utils_dir):
-        print("Missing ",FNAMES.Utils_dir,"directory, check your install. Exiting.")
-        sys.exit()   
+        print("Missing ", FNAMES.Utils_dir, "directory, check your install. Exiting.")
+        sys.exit()
     for directory in (FNAMES.Preview_dir, FNAMES.Provider_dir, FNAMES.Extent_dir, FNAMES.Filter_dir, FNAMES.OSM_dir,
-                      FNAMES.Mask_dir,FNAMES.Imagery_dir,FNAMES.Elevation_dir,FNAMES.Geotiff_dir,FNAMES.Patch_dir,
-                      FNAMES.Tile_dir,FNAMES.Tmp_dir):
+                      FNAMES.Mask_dir, FNAMES.Imagery_dir, FNAMES.Elevation_dir, FNAMES.Geotiff_dir, FNAMES.Patch_dir,
+                      FNAMES.Tile_dir, FNAMES.Tmp_dir):
         if not os.path.isdir(directory):
-            try: 
+            try:
                 os.makedirs(directory)
-                print("Creating missing directory",directory)
-            except: 
-                print("Could not create required directory",directory,". Exit.")
+                print("Creating missing directory", directory)
+            except:
+                print("Could not create required directory", directory, ". Exit.")
                 sys.exit()
     IMG.initialize_extents_dict()
     IMG.initialize_color_filters_dict()
     IMG.initialize_providers_dict()
     IMG.initialize_combined_providers_dict()
-    if len(sys.argv)==1: # switch to the graphical interface
+    if len(sys.argv) == 1:  # switch to the graphical interface
         Ortho4XP = GUI.Ortho4XP_GUI()
-        Ortho4XP.mainloop()	    
+        Ortho4XP.mainloop()
         print("Bon vol!")
-    else: # sequel is only concerned with command line 
-        if len(sys.argv)<3:
+    else:  # sequel is only concerned with command line
+        if len(sys.argv) < 3:
             print(cmd_line); sys.exit()
         try:
-            lat=int(sys.argv[1])
-            lon=int(sys.argv[2])
+            lat = int(sys.argv[1])
+            lon = int(sys.argv[2])
         except:
             print(cmd_line); sys.exit()
-        if len(sys.argv)==3:
+        if len(sys.argv) == 3:
             try:
-                tile=CFG.Tile(lat,lon,'')
+                tile = CFG.Tile(lat, lon, '')
             except Exception as e:
                 print(e)
                 print("ERROR: could not read tile config file."); sys.exit()
         else:
             try:
-                provider_code=sys.argv[3]
-                zoomlevel=int(sys.argv[4])
-                tile=CFG.Tile(lat,lon,'')
-                tile.default_website=provider_code
-                tile.default_zl=zoomlevel
+                provider_code = sys.argv[3]
+                zoomlevel = int(sys.argv[4])
+                tile = CFG.Tile(lat, lon, '')
+                tile.default_website = provider_code
+                tile.default_zl = zoomlevel
             except:
                 print(cmd_line); sys.exit()
         try:
@@ -80,5 +82,3 @@ if __name__ == '__main__':
             print("Bon vol!")
         except:
             print("Crash!")
- 
-        
